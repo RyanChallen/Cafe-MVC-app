23269d73592441b0d5e5264643b9210c394ca6ac
Ryan Challen <ryanchallen@outlook.com>
2020-01-17 14:13:43 +1000

    Testing post methods

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,7 +1,24 @@
 class MenuController < ApplicationController
 
+    before_action :setup_data
+
     def index
-        render plain: "Meow"
+        # render plain: "Meow"
+        #render html: "<h1>Le Menu</h1>".html_safe
+        render json: @data
+    end
+
+    def create
+        render plain: "stuff and things"
+    end
+
+    private
+    def setup_data
+        @data = [
+            {item: "tea", price: 3, quantity: 0},
+            {item: "coffee", price: 4, quantity: 10},
+            {item: "scone", price: 5, quantity: 20}
+        ]
     end
 
 end
